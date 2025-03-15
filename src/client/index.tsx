8d16514de3f3e5b1186e6e5852850dc76adfea7f
dgubz <120327308+dgubz@users.noreply.github.com>
2025-03-15 17:12:00 -0500

    Update index.tsx

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -10,34 +10,60 @@ import {
 } from "react-router";
 import { nanoid } from "nanoid";
 
+import { names, type ChatMessage, type Message } from "../shared";
+
 function App() {
-  const [name] = useState(() => {
-    let userName = "";
-    while (!userName) {
-      userName = prompt("Enter your name:")?.trim() || "";
-    }
-    return userName;
-  });
-  
-  const [messages, setMessages] = useState([]);
+  const [name] = "user" + Math.random();
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
   const { room } = useParams();
 
   const socket = usePartySocket({
     party: "chat",
     room,
     onMessage: (evt) => {
-      const message = JSON.parse(evt.data);
+      const message = JSON.parse(evt.data as string) as Message;
       if (message.type === "add") {
         const foundIndex = messages.findIndex((m) => m.id === message.id);
         if (foundIndex === -1) {
-          setMessages((messages) => [...messages, message]);
+          // probably someone else who added a message
+          setMessages((messages) => [
+            ...messages,
+            {
+              id: message.id,
+              content: message.content,
+              user: message.user,
+              role: message.role,
+            },
+          ]);
         } else {
+          // this usually means we ourselves added a message
+          // and it was broadcasted back
+          // so let's replace the message with the new message
           setMessages((messages) => {
-            return messages.map((m) =>
-              m.id === message.id ? message : m
-            );
+            return messages
+              .slice(0, foundIndex)
+              .concat({
+                id: message.id,
+                content: message.content,
+                user: message.user,
+                role: message.role,
+              })
+              .concat(messages.slice(foundIndex + 1));
           });
         }
+      } else if (message.type === "update") {
+        setMessages((messages) =>
+          messages.map((m) =>
+            m.id === message.id
+              ? {
+                  id: message.id,
+                  content: message.content,
+                  user: message.user,
+                  role: message.role,
+                }
+              : m,
+          ),
+        );
       } else {
         setMessages(message.messages);
       }
@@ -57,19 +83,24 @@ function App() {
         onSubmit={(e) => {
           e.preventDefault();
           const content = e.currentTarget.elements.namedItem(
-            "content"
+            "content",
           ) as HTMLInputElement;
-          if (!content.value.trim()) return;
-          
-          const chatMessage = {
+          const chatMessage: ChatMessage = {
             id: nanoid(8),
             content: content.value,
             user: name,
             role: "user",
           };
-          
           setMessages((messages) => [...messages, chatMessage]);
-          socket.send(JSON.stringify({ type: "add", ...chatMessage }));
+          // we could broadcast the message here
+
+          socket.send(
+            JSON.stringify({
+              type: "add",
+              ...chatMessage,
+            } satisfies Message),
+          );
+
           content.value = "";
         }}
       >
@@ -77,7 +108,7 @@ function App() {
           type="text"
           name="content"
           className="ten columns my-input-text"
-          placeholder="Chat"
+          placeholder={Chat}
           autoComplete="off"
         />
         <button type="submit" className="send-message two columns">
@@ -88,12 +119,13 @@ function App() {
   );
 }
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Navigate to={`/${nanoid()}`} />} />
+      <Route path="/" element={<Navigate to={/${nanoid()}} />} />
       <Route path="/:room" element={<App />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter>,
 );
